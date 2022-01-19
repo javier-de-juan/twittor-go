ff0d36dcc7cb1640feadcaac494df4877f5a43a8
Javier De Juan Trujillo <tools@javierdejuan.es>
2022-01-19 09:35:06 +0100

    feat: Add default handlers module

---
+++ handlers/handlers.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Handle() {
+	router  := mux.NewRouter()
+	PORT    := getPort()
+	handler := cors.AllowAll().Handler(router)
+
+	log.Fatal(http.ListenAndServe(":"+PORT, handler))
+}
+
+func getPort() string {
+	PORT := os.Getenv("PORT")
+
+	if PORT == "" {
+		PORT = "8080"
+	}
+
+	return PORT
+}
