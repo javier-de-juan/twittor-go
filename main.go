e9927903eccbb64f542d94540681ddad68111e3f
Javier De Juan Trujillo <tools@javierdejuan.es>
2022-01-19 09:46:20 +0100

    feat: Default main file

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/javier-de-juan/twittor-go/bd"
+	"github.com/javier-de-juan/twittor-go/handlers"
+	"log"
+)
 
+func main() {
+	if ! bd.IsConnected() {
+		log.Fatal("Database is not connected")
+		return
+	}
+
+	handlers.Handle()
 }
