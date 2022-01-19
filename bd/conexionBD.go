acb47d41dac88810e14ce84cf4e658de01d8b4c0
Javier De Juan Trujillo <tools@javierdejuan.es>
2022-01-19 09:46:04 +0100

    feat: Default connection to mongo

---
+++ bd/conexionBD.go
@@ -0,0 +1,54 @@
+package bd
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+)
+
+var MongoCN = ConnectToDataBase()
+var clientOptions = getClientOptions()
+
+func ConnectToDataBase() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+
+	// Totalmente innecesario
+	pingErr := client.Ping(context.TODO(), nil)
+
+	if pingErr != nil {
+		log.Fatal(pingErr.Error())
+		return client
+	}
+
+	log.Println("Connected to DB")
+
+	return client
+}
+
+func IsConnected() bool {
+	pingErr := MongoCN.Ping(context.TODO(), nil)
+
+	if pingErr != nil {
+		return false
+	}
+
+	return true
+}
+
+func getClientOptions() *options.ClientOptions {
+	connectionURI := os.Getenv("MONGO_URI")
+
+	if connectionURI == "" {
+		log.Fatal("Mongo connection uri was not found.")
+		return options.Client()
+	}
+
+	return options.Client().ApplyURI(connectionURI)
+}
