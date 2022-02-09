567606da9907894f3e1b380a22a8253c5d586b8c
Javier De Juan Trujillo <tools@javierdejuan.es>
2022-02-09 09:20:32 +0100

    feat: Add get tweers function for tweet repository

--- bd/TweetRepository.go
+++ bd/TweetRepository.go
@@ -3,10 +3,15 @@ package bd
 import (
 	"context"
 	"github.com/javier-de-juan/twittor-go/models"
+	"github.com/javier-de-juan/twittor-go/models/responseModel"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	mongoOptions "go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 )
 
 const tweetCollection = "tweets"
+const rowsPerPage = 20
 
 func SaveTweet(tweet models.Tweet) (string, bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), getMaxExecutionTime())
@@ -15,7 +20,7 @@ func SaveTweet(tweet models.Tweet) (string, bool, error) {
 
 	db := MongoCN.Database(dbName)
 	dbCollection := db.Collection(tweetCollection)
-	
+
 	result, err := dbCollection.InsertOne(ctx, tweet)
 
 	if err != nil {
@@ -25,4 +30,47 @@ func SaveTweet(tweet models.Tweet) (string, bool, error) {
 	tweetID, _ := result.InsertedID.(primitive.ObjectID)
 
 	return tweetID.String(), true, nil
+}
+
+func GetTweetsFromUser(ID string, page int64) ([]*responseModel.Tweet, bool) {
+	ctx, cancel := context.WithTimeout(context.Background(), getMaxExecutionTime())
+
+	defer cancel()
+
+	db := MongoCN.Database(dbName)
+	dbCollection := db.Collection(tweetCollection)
+
+	var tweets []*responseModel.Tweet
+
+	filter := bson.M{
+		"user_id": ID,
+	}
+
+	options := mongoOptions.Find()
+	options.SetLimit(rowsPerPage)
+	options.SetSort(bson.D{{Key: "created_at", Value: -1}})
+	options.SetSkip((page - 1) * rowsPerPage)
+
+	cursor, err := dbCollection.Find(ctx, filter, options)
+
+	if err != nil {
+		log.Fatal(err.Error())
+
+		return tweets, false
+	}
+
+	// No me interesa un contexto. Con "TODO" lo que puedo hacer es esperar indefinidamente
+	for cursor.Next(context.TODO()){
+		var tweet responseModel.Tweet
+		err := cursor.Decode(&tweet)
+
+		if err != nil {
+			log.Fatal(err.Error())
+			continue
+		}
+
+		tweets = append(tweets, &tweet)
+	}
+
+	return tweets, true
 }
